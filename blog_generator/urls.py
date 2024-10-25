563fff31873f199ffbbf66f9d1ac50933c0b5470
Puni2001 <punithpunith2001@gmail.com>
2024-10-25 10:07:42 +0530

    'fixed api error "

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog_generator'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('generate-blog/', views.generate_blog, name='generate-blog'),
+    path('generate-blog', views.generate_blog, name='generate-blog'),
     path('blog-list/', views.blog_list, name='blog-list'),
     path('blog-details/<int:pk>/', views.blog_details, name='blog-details'),
     path("delete-selected-blogs/", views.delete_selected_blogs, name="delete-selected-blogs"),
