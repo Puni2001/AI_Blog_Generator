2816770292fdccd3b36e02ce2a2efe5fa5505a5c
Puni2001 <punithpunith2001@gmail.com>
2024-11-23 15:56:37 +0530

    "new|"

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -71,20 +71,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-        ssl_require=True,
-    )
-}
 # DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL'),
+#         conn_max_age=600,
+#         conn_health_checks=True,
+#         ssl_require=True,
+#     )
+# }
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Database for local development ENABLE THIS FOR LOCAL DEVELOPMENT
 # if os.getenv('VERCEL_ENV', None):
