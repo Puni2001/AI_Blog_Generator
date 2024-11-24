e12d92f57429deefb5b9d28a9f6ba0df2c6590b0
Puni2001 <punithpunith2001@gmail.com>
2024-11-24 11:23:00 +0530

    "UPDATED"

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Create a virtual environment
-python3 -m venv venv
-source venv/bin/activate
-# Update pip
-echo "Updating pip..."
-python3 -m pip install --upgrade pip
-
 # Install dependencies
 echo "Installing project dependencies..."
-python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Creating favicon..."
