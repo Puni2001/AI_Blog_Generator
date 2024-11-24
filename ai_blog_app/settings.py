c089d053f9262ef7d331469945732b50563033ad
Puni2001 <punithpunith2001@gmail.com>
2024-11-24 11:41:10 +0530

    "new"

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -71,14 +71,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-        ssl_require=True,
-    )
-}
+        'default': dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        )
+    }
+
 # DATABASES = {
 #         'default': {
 #             'ENGINE': 'django.db.backends.sqlite3',
@@ -86,18 +88,6 @@ DATABASES = {
 #         }
 #     }
 
-# Database for local development ENABLE THIS FOR LOCAL DEVELOPMENT
-# if os.getenv('VERCEL_ENV', None):
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:
-#     DATABASES ={
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
-# }
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Update pip
+echo "Updating pip..."
+python3 -m pip install --upgrade pip
+
 # Install dependencies
 echo "Installing project dependencies..."
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Creating favicon..."
