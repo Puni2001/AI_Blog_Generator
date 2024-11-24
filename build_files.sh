15b305d4fab8ac2ad4593b28388678e39b026d9d
Puni2001 <punithpunith2001@gmail.com>
2024-11-24 12:20:54 +0530

    f "fix_this"

--- build_files.sh
+++ build_files.sh
@@ -4,15 +4,15 @@
 echo "Updating pip..."
 python3 -m pip install --upgrade pip
 
-# Install dependencies
+# Install project dependencies
 echo "Installing project dependencies..."
-python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
+# Handle the missing PIL issue for favicon creation (make sure Pillow is installed in requirements.txt)
 echo "Creating favicon..."
 python3 create_favicon.py
 
-# Collect staticfiles
+# Collect static files for Django project
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
 
