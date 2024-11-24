aa7fee66b38674a070a90854c1e55f533d17fb7a
Puni2001 <punithpunith2001@gmail.com>
2024-11-24 11:25:32 +0530

    "fix"

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,14 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # # Install dependencies
-# # echo "Installing project dependencies..."
-# # pip install -r requirements.txt
+# Install dependencies
+echo "Installing project dependencies..."
+pip install -r requirements.txt
 
-# echo "Creating favicon..."
-# python3 create_favicon.py
+echo "Creating favicon..."
+python3 create_favicon.py
 
-# # Collect staticfiles
-# echo "Collecting static files..."
-# python3 manage.py collectstatic --noinput --clear
+# Collect staticfiles
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput --clear
 
-# echo "Build process completed!"
+echo "Build process completed!"
